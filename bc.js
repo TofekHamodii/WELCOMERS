3acea399b0f82a08f48282d3f5355198b716cd5c
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-14 20:21:10 +0200

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,10 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('guildMemberAdd' , member => {
+var server = "511871944958803968"; 
+var channel = "511875595773149185";
+client.guilds.get(server).channels.get(channel).send("**Welcome To Neptune ,. :champagne_glass: **")
+});
+
+client.login("NTA3OTc4Mjg4MjUwMDkzNTY4.DsyoZQ.VhSBHpcHJwS54_TWlHMrz6DxkkY"); 
